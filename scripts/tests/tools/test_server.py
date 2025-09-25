b206a7b34a26c76a83ae5b509899386049b511e1
Your Name <you@example.com>
2025-09-25 15:04:58 -0500

    testing retreival tool server

--- scripts/tests/tools/test_server.py
+++ scripts/tests/tools/test_server.py
@@ -7,4 +7,10 @@ if __name__ == "__main__":
         params={"query": "What is the main character doing?", "topk": 5, "video_id": "fvCrE5NCsts"},
         timeout=30,
     )
+    
+    response = requests.get(
+        "http://127.0.0.1:9000/search_subtitle",
+        params={"query": "Hayha regularly participated\nin these competitions,", "topk": 2, "video_id": "fvCrE5NCsts"},
+        timeout=30,
+    )
     print(response.json())
