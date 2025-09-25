fbb4d823b55a5625585d787627a1da967b43233c
Your Name <you@example.com>
2025-09-25 15:04:07 -0500

    hydra config for data pipeline

--- scripts/data_ops/run_data_pipeline.py
+++ scripts/data_ops/run_data_pipeline.py
@@ -1,5 +1,14 @@
 from data.lvb import LongVideoBench
+import hydra
+from omegaconf import DictConfig
+
+
+@hydra.main(version_base=None, config_path="../../src/vseek/config/retriever", config_name="config")
+def main(cfg: DictConfig):
+    dataset = LongVideoBench(cfg)
+    dataset.save_it_as_vseek_data(desired_interval_in_sec=cfg.dataset.desired_interval_in_sec)
+    # dataset.fix_subtitle_embeddings()
+
 
 if __name__ == "__main__":
-    dataset = LongVideoBench()
-    dataset.save_it_as_vseek_data(desired_interval_in_sec=1)
+    main()
