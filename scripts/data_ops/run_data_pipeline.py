2e971261bcfb5e153f8e98611109c66708e322c7
Your Name <you@example.com>
2025-11-15 18:56:32 -0600

    data pipeline for multiple datasets

--- scripts/data_ops/run_data_pipeline.py
+++ scripts/data_ops/run_data_pipeline.py
@@ -1,12 +1,26 @@
 from data.lvb import LongVideoBench
+from data.lvbench import LVBench
+from data.videomme import VideoMME
+from data.mlvu import MLVU
+
 import hydra
 from omegaconf import DictConfig
 
 
 @hydra.main(version_base=None, config_path="../../src/vseek/config/retriever", config_name="config")
 def main(cfg: DictConfig):
-    dataset = LongVideoBench(cfg)
-    dataset.save_it_as_vseek_data(desired_interval_in_sec=cfg.dataset.desired_interval_in_sec)
+    if cfg.dataset.name == "lvb":
+        dataset = LongVideoBench(cfg)
+        dataset.save_it_as_vseek_data(desired_interval_in_sec=cfg.dataset.desired_interval_in_sec)
+    elif cfg.dataset.name == "lvbench":
+        dataset = LVBench(cfg)
+        dataset.save_it_as_vseek_data(desired_interval_in_sec=cfg.dataset.desired_interval_in_sec)
+    elif cfg.dataset.name == "videomme":
+        dataset = VideoMME(cfg)
+        dataset.save_it_as_vseek_data(desired_interval_in_sec=cfg.dataset.desired_interval_in_sec)
+    elif cfg.dataset.name == "mlvu":
+        dataset = MLVU(cfg)
+        dataset.save_it_as_vseek_data(desired_interval_in_sec=cfg.dataset.desired_interval_in_sec)
     # dataset.fix_subtitle_embeddings()
 
 
